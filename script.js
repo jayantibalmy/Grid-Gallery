fb55705b81a7f4d9ecbe0a2b4c01371675747062
jbalmy <108647411+miniisokay@users.noreply.github.com>
2023-10-01 23:58:29 +0530

    add dummy puzzle

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/* home page/ index.html */
-
-/*nav bar */
+/*Common nav bar */
 document.addEventListener("DOMContentLoaded", function () {
     const navbarContainer = document.getElementById("navbar-container");
     if (navbarContainer) {
@@ -12,27 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => {
                 console.error("Error loading navigation bar:", error);
             });
-    }
-});
-
-//puzzle 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const nonogram = document.querySelector('.nonogram');
-
-    // Create a 5x5 grid
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.addEventListener('click', toggleCell);
-            nonogram.appendChild(cell);
-        }
-    }
-
-    function toggleCell() {
-        // Toggle cell between filled and empty
-        this.classList.toggle('filled');
     }
 });
 
